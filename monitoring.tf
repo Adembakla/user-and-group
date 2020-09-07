8d5396837e7df1a587b46f0985bc9cddb302a4e4
Adembakla <68790358+Adembakla@users.noreply.github.com>
2020-09-07 14:29:58 -0500

    Update monitoring.tf

--- monitoring.tf
+++ monitoring.tf
@@ -1,38 +1,40 @@
-resource "aws_iam_group_membership" "monadmin" {
-  name = "monadmin-membership"
-
+resource "aws_iam_group_membership" "team3" {
+  name = "tf-testing-group3-membership"
   users = [
-    aws_iam_user.monadmin1.name,
-    aws_iam_user.monadmin2.name,
-    aws_iam_user.monadmin3.name,
-    aws_iam_user.monadmin4.name,
+    aws_iam_user.monitor1.name,
+    aws_iam_user.monitor2.name,
+    aws_iam_user.monitor3.name,
+    aws_iam_user.monitor4.name,
   ]
-
-  group = aws_iam_group.monadmin.name
+  group = aws_iam_group.monitoring.name
 }
-
-resource "aws_iam_group" "monadmin" {
-  name = "monadmin"
+resource "aws_iam_group" "monitoring" {
+  name = "monitoring"
 }
-
-resource "aws_iam_user" "monadmin1" {
-  name = "monadmin1"
+resource "aws_iam_user" "monitor1" {
+  name = "monitor1"
 }
-
-resource "aws_iam_user" "monadmin2" {
-  name = "monadmin2"
+resource "aws_iam_user" "monitor2" {
+  name = "monitor2"
 }
-resource "aws_iam_user" "monadmin3" {
-  name = "monadmin3"
+resource "aws_iam_user" "monitor3" {
+  name = "monitor3"
 }
-
-resource "aws_iam_user" "monadmin4" {
-  name = "monadmin4"
+resource "aws_iam_user" "monitor4" {
+  name = "monitor4"
 }
-
-resource "aws_iam_policy_attachment" "testmon-attach" {
-  name       = "testmon-attachment"
-  users      = [aws_iam_user.monadmin1.name,aws_iam_user.monadmin2.name,aws_iam_user.monadmin3.name,aws_iam_user.monadmin4.name]
-  groups     = [aws_iam_group.monadmin.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess,arn:aws:iam::aws:policy/AmazonEC2FullAccess,arn:aws:iam::aws:policy/IAMFullAccess"
+resource "aws_iam_policy_attachment" "test-attach3" {
+  name       = "test-attachment3"
+  groups     = [aws_iam_group.monitoring.name]
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+resource "aws_iam_policy_attachment" "test-attach4" {
+  name       = "test-attachment4"
+  groups     = [aws_iam_group.monitoring.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+resource "aws_iam_policy_attachment" "test-attach5" {
+  name       = "test-attachment5"
+  groups     = [aws_iam_group.monitoring.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
